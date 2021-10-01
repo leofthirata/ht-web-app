69746c20c71c8839c5d0d8c476e08e0e1742dc0e
Leonardo Hirata <leonardo.hirata@hausenn.com.br>
2021-10-01 10:32:26 -0300

    add keycloak

--- src/app/pages/backend-info/backend-info.page.ts
+++ src/app/pages/backend-info/backend-info.page.ts
@@ -1,17 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import * as backend from '../../home/services/backend/backend';
+import Keycloak from 'keycloak-js';
 
 @Component({
   selector: 'app-backend-info',
   templateUrl: './backend-info.page.html',
   styleUrls: ['./backend-info.page.scss'],
 })
+
 export class BackendInfoPage implements OnInit {
 
-  constructor() { }
+  private keycloak;
+
+  constructor() { 
+    this.keycloak = Keycloak({
+      url: "https://pado.hausenn.com.br/auth/",
+      realm: "hausenn",
+      clientId: 'pado-client-app'
+    });
+
+    this.keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
+        console.log(authenticated);
+    })
+  }
 
   ngOnInit() {
   }
 
-  
+  public logoutOnClick() {
+    this.keycloak.logout();
+  }
 }
